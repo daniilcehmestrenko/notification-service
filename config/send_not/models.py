2ee1d620e57ab19e839c7c93de92753bd2dd1515
daniil <daniilcehmestrenko@icloud.com>
2022-12-16 21:36:30 +0300

    add models

--- config/send_not/models.py
+++ config/send_not/models.py
@@ -3,30 +3,19 @@ from django.db import models
 
 
 class MailingList(models.Model):
-    name = models.CharField(
-            max_length=20,
-            verbose_name='Название рассылки'
+    date_start = models.DateTimeField(
+            verbose_name='Дата и время начала рассылки'
         )
-    start_date = models.DateTimeField(
-            verbose_name='Дата и время запуска рассылки'
-        )
-    message = models.ForeignKey(
-            'Message',
-            on_delete=models.DO_NOTHING,
-            related_name='message',
-            verbose_name='Cообщение'
+    text = models.TextField(
+            verbose_name='Текст рассылки'
         )
     client_filter = models.CharField(
             max_length=20,
-            verbose_name='Код оператора или тег',
-            help_text='По данному полю будут фильтроваться клиенты'
+            verbose_name='Фильтр клиентов'
         )
-    expiration_date = models.DateTimeField(
+    date_end = models.DateTimeField(
             verbose_name='Дата и время окончания рассылки'
         )
-    
-    def __str__(self):
-        return self.name
 
     class Meta:
         verbose_name = 'Рассылка'
@@ -36,8 +25,7 @@ class MailingList(models.Model):
 class Client(models.Model):
     phone = models.CharField(
             max_length=11,
-            verbose_name='Номер телефона',
-            help_text='формат:7XXXXXXXXXX',
+            verbose_name='Телефон',
             validators=[
                 RegexValidator(regex=r'7\d{10}')
             ]
@@ -46,31 +34,37 @@ class Client(models.Model):
             max_length=10,
             verbose_name='Код оператора'
         )
-    timezone = models.CharField(
-            max_length=10,
-            verbose_name='Часовой пояс',
-        )
     tag = models.CharField(
             max_length=50,
             verbose_name='Произвольная метка'
         )
-
-    def __str__(self):
-        return self.phone
+    time_zone = models.CharField(
+            max_length=10,
+            help_text='Формат: UCT+01:00'
+            verbose_name='Часовой пояс'
+        )
 
     class Meta:
         verbose_name = 'Клиент'
-        verbose_name_plural='Клиенты'
+        verbose_name_plural = 'Клиенты'
 
 
 class Message(models.Model):
-    text = models.TextField(
-            verbose_name='Текст сообщения'
+    date_create = models.DateTimeField(
+            auto_now=True,
+            verbose_name='Дата создания'
         )
-
-    def __str__(self):
-        return str(self.pk)
-
-    class Meta:
-        verbose_name = 'Сообщение'
-        verbose_name_plural = 'Сообщения'
+    status = models.BooleanField(
+            verbose_name='Статус отправки'
+        )
+    mailing_list = models.ForeignKey(
+            'MailingList',
+            on_delete=models.DO_NOTHING,
+            related_name='messages',
+            verbose_name='Рассылка'
+        )
+    clients = models.ManyToManyField(
+            'Client',
+            related_name='clients',
+            verbose_name='Клиенты'
+        )
